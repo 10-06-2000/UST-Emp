cebf7da46fbca71e3fc0841d2f40a2899b044efc
Athulya Vijayan <athulyavijayan20@gmail.com>
2023-03-31 15:52:08 +0530

    "modified files"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeRegService } from './employee-reg.service';
+import { EmployeeRegComponent } from './employee-reg/employee-reg.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeRegComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeRegService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
